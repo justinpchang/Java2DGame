eb784529639bd6f7e2ca5a86135ba937b76a17f5
justinpchang <justin.p.chang@gmail.com>
2014-09-03 13:01:43 -0400

    Added sprite sheet and template. implemented into code

--- code/src/io/github/justinpchang/game/Game.java
+++ code/src/io/github/justinpchang/game/Game.java
@@ -1,8 +1,15 @@
 package io.github.justinpchang.game;
 
+import io.github.justinpchang.game.gfx.SpriteSheet;
+
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -18,6 +25,12 @@ public class Game extends Canvas implements Runnable{
 	private JFrame frame;
 	
 	public boolean running = false;
+	public int tickCount = 0;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	
+	private SpriteSheet spriteSheet = new SpriteSheet("/sprite_sheet.png");
 	
 	public Game() {
 		
@@ -65,8 +78,35 @@ public class Game extends Canvas implements Runnable{
 		
 		while(running) {
 			
-			tick();
-			render();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+			
+			while(delta >= 1) {
+				ticks++;
+				tick();
+				delta -= 1;
+				shouldRender = true;
+			}
+			
+			try {
+				Thread.sleep(2);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+				
+			if(shouldRender) {
+				frames++;
+				render();
+			}
+			
+			if(System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println(frames + " frames, " + ticks + " ticks");
+				frames = 0;
+				ticks = 0;
+			}
 			
 		}
 		
@@ -74,13 +114,30 @@ public class Game extends Canvas implements Runnable{
 	
 	public void tick() {
 		
+		tickCount++;
 		
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = i + tickCount;
+		}
 		
 	}
 	
 	public void render() {
 		
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
 		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth(), getHeight());
+		
+		g.dispose();
+		bs.show();
 		
 	}
 	
